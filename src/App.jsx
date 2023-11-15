e46bc794e4e5fa9c9c3b3732b2c9c21a5958c219
zlAxel <axel.macias17@hotmail.com>
2023-11-14 22:30:22 -0600

    se sube inicio de sesión y configuración de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import LayoutAuth from "./layouts/LayoutAuth"
+import Login from "./views/Auth/Login"
+
 function App() {
+  const Home = () => <h1>Home</h1>
+  const Register = () => <h1>Register</h1>
+
   return (
-    <>
-      <h1>Hello, world!</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      
+      <Route path="/auth" element={<LayoutAuth />} >
+        <Route index element={<Navigate to="login" />} />
+
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Route>
+      
+    </Routes>
+
   )
 }
 
